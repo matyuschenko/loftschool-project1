f349447f6768bfb61082811f3d19fcb43d3fe682
Maxim Matyuschenko <matyuschenko@gmail.com>
2016-10-09 21:56:20 +0300

    js: add blog small menu

--- source/js/blogmenu.js
+++ source/js/blogmenu.js
@@ -15,18 +15,21 @@ $(window).on('load', function() {
             $(window).on('scroll', function () {
                 var y_scroll_pos = window.pageYOffset,
                     fixed_nav_top_margin = 30;
-
-                if (y_scroll_pos > blog_nav_scroll_pos - fixed_nav_top_margin) {
-                    blog_nav.css({
-                        position: 'fixed',
-                        top: fixed_nav_top_margin,
-                        left: blog_nav_left_offset,
-                        width: blog_nav_w
-                    });
-                } else {
-                    blog_nav.css({
-                        position: 'static'
-                    });
+                
+                if ($(window).width() > 768) {
+                    console.log(1);
+                    if (y_scroll_pos > blog_nav_scroll_pos - fixed_nav_top_margin) {
+                        blog_nav.css({
+                            position: 'fixed',
+                            top: fixed_nav_top_margin,
+                            left: blog_nav_left_offset,
+                            width: blog_nav_w
+                        });
+                    } else {
+                        blog_nav.css({
+                            position: 'static'
+                        });
+                    }
                 }
 
                 for (var o in article_offsets) {
@@ -38,6 +41,35 @@ $(window).on('load', function() {
                     }
                 }
             });
+
+            if ($(window).width() <= 768) {
+                _addSmallMenuBehavior();
+            }
+
+            _addListenerOnResize();
+        },
+        _addListenerOnResize = function () {
+            $(window).on('resize', function () {
+                if ($(window).width() <= 768) {
+                    _addSmallMenuBehavior();
+                }
+            })
+        },
+        _addSmallMenuBehavior = function () {
+            var blog_sidebar = $('.blog-sidebar');
+            $('.small-screen-button').on('click', function () {
+                if (blog_sidebar.hasClass('shown')) {
+                    blog_sidebar.animate({
+                        left: '-33.3%'
+                    }, 150);
+                    blog_sidebar.removeClass('shown');
+                } else {
+                    blog_sidebar.animate({
+                        left: 0
+                    }, 150);
+                    blog_sidebar.addClass('shown');
+                }
+            })
         };
 
         return {init: init};
